1fe1ad438bec590487a0560216897887743c1707
francosta <francisco@fcosta.pt>
2019-08-19 22:11:01 +0100

    Added timestamps to user model.

--- src/models/user.js
+++ src/models/user.js
@@ -2,26 +2,29 @@
 
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String
+    },
+    lastname: {
+      type: String
+    },
+    email: {
+      type: String
+    },
+    age: {
+      type: Number
+    },
+    password: {
+      type: String
+    },
+    profilePicture: {
+      type: Buffer
+    }
   },
-  lastname: {
-    type: String
-  },
-  email: {
-    type: String
-  },
-  age: {
-    type: Number
-  },
-  password: {
-    type: String
-  },
-  profilePicture: {
-    type: Buffer
-  }
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
